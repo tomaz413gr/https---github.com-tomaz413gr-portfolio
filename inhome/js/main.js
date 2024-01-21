ac53d394d16887b792db50f26c5e34ff1954b341
tomaz413gr <123848400+tomaz413gr@users.noreply.github.com>
2024-01-21 15:10:07 +1100

    Applications_finished

--- inhome/js/main.js
+++ inhome/js/main.js
@@ -49,8 +49,8 @@ $(document).ready(function () {
         slidesToShow: 1,
         slidesToScroll: 1,
         arrows: true,
-        // autoplay:true,
-        // autoplaySpeed: 3000,
+        autoplay:true,
+        autoplaySpeed: 3000,
         responsive: [
             {
                 breakpoint: 1510,
